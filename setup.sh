5f12c40f79fd5cf31329f290ff4811c1b7b5e6f9
Simon Ljungbeck <simon.ljungbeck@gmail.com>
2025-01-01 20:00:54 +0100

    feat: create symlink to global gitignore in home directory

--- setup.sh
+++ setup.sh
@@ -3,9 +3,16 @@
 # Get the directory of the current script
 SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 
-# Set the Git global excludes file to the retrieved directory
-GIT_EXCLUDES_FILE="$SCRIPT_DIR/gitignore_global"
-git config --global core.excludesfile "$GIT_EXCLUDES_FILE"
+# Define the Git excludes file and the symlink in the home directory
+GIT_EXCLUDES_FILE="$SCRIPT_DIR/.gitignore_global"
+SYMLINK_PATH="$HOME/.gitignore_global"
 
-# Print confirmation message
-echo "Git global excludes file set to: $GIT_EXCLUDES_FILE"
+# Create a symlink to the Git excludes file in the home directory
+ln -sf "$GIT_EXCLUDES_FILE" "$SYMLINK_PATH"
+
+# Set the Git global excludes file to the symlink in the home directory
+git config --global core.excludesfile "$SYMLINK_PATH"
+
+# Print confirmation messages
+echo "Symlink created: $SYMLINK_PATH -> $GIT_EXCLUDES_FILE"
+echo "Git global excludes file set to: $SYMLINK_PATH"
